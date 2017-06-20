24dd7eec5ee362eb20390b58f0034fa42a566e99
Trisha Salas <trisha@trishasalas.com>
2017-06-20 12:14:05 -0500

    reconfig autoload

--- functions.php
+++ functions.php
@@ -9,6 +9,12 @@ define( 'TRISHASALAS_VERSION', '2.0.0' );
 define( 'TRISHASALAS_DIR', __DIR__ );
 define( 'TRISHASALAS_URL', get_template_directory_uri() );
 
-require_once __DIR__ . '/vendor/aaronholbrook/autoload/autoload.php';
 
-\AaronHolbrook\Autoload\autoload( __DIR__ . '/includes' );
+require_once __DIR__ . '/includes/custom-login.php';
+require_once __DIR__ . '/includes/customizer.php';
+require_once __DIR__ . '/includes/enqueue.php';
+require_once __DIR__ . '/includes/extras.php';
+require_once __DIR__ . '/includes/jetpack.php';
+require_once __DIR__ . '/includes/setup.php';
+require_once __DIR__ . '/includes/sidebars.php';
+require_once __DIR__ . '/includes/template-tags.php';
