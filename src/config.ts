f716411e72261ae9fa91af5cdc0a09921a576cac
Trisha Salas <trisha@trishasalas.com>
2025-09-29 13:04:28 -0500

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,24 @@
+export const SITE = {
+  website: "https://trishasalas.com/", // replace this with your deployed domain
+  author: "Trisha Salas",
+  profile: "",
+  desc: "",
+  title: "Trisha Salas",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 5,
+  postPerPage: 5,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: true,
+    text: "Edit page",
+    url: "https://github.com/satnaing/astro-paper/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "America/Chicago", // Default global timezone (IANA format)
+  // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
---
+++ src/constants.ts
@@ -0,0 +1,42 @@
+import type { Props } from "astro";
+import IconMail from "@/assets/icons/IconMail.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import { SITE } from "@/config";
+
+interface Social {
+  name: string;
+  href: string;
+  linkTitle: string;
+  icon: (_props: Props) => Element;
+}
+
+export const SOCIALS: Social[] = [
+  {
+    name: "GitHub",
+    href: "https://github.com/trishasalas/",
+    linkTitle: `${SITE.title} on GitHub`,
+    icon: IconGitHub,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/trishasalas/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    icon: IconLinkedin,
+  },
+  {
+    name: "Mail",
+    href: "mailto:trisha@trishasalas.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    icon: IconMail,
+  },
+] as const;
+
+export const SHARE_LINKS: Social[] = [
+  {
+    name: "Mail",
+    href: "mailto:?subject=See%20this%20post&body=",
+    linkTitle: `Share this post via email`,
+    icon: IconMail,
+  },
+] as const;
