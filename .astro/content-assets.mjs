e8a85522d8df69e00022c121990bb5a9a81c2848
Trisha Salas <trisha@trishasalas.com>
2025-11-02 09:16:19 -0600

    remove .astro dir from .gitignore

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_PEvjh from "../../assets/images/ai-inspo.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fai-as-an-accommodation.md";
+export default new Map([["../../assets/images/ai-inspo.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fai-as-an-accommodation.md", __ASTRO_IMAGE_IMPORT_PEvjh]]);
+		
